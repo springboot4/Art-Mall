bbb47da8b3d41e7341a9083822b20ebb03df19ca
Fxz <2235602974@qq.com>
2022-05-14 00:23:37 +0800

    :fire: mall瘦身，删除fxz-cloud文件

---
+++ mall-order/mall-order-biz/src/main/java/com/fxz/mall/order/FxzMallOrderApplication.java
@@ -0,0 +1,24 @@
+package com.fxz.mall.order;
+
+import com.fxz.common.security.annotation.EnableFxzCloudResourceServer;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author fxz
+ */
+@EnableFxzCloudResourceServer
+@EnableFeignClients(basePackages = {"com.fxz"})
+@MapperScan("com.fxz")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FxzMallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxzMallOrderApplication.class, args);
+    }
+
+}
