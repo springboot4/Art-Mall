0b0dc9f758565e7065c46d380eee1aa44d2da5ee
Fxz <2235602974@qq.com>
2022-05-17 20:32:37 +0800

    :sparkles: 购物车结算

--- mall-order/mall-order-biz/src/main/java/com/fxz/mall/order/controller/app/OrderController.java
+++ mall-order/mall-order-biz/src/main/java/com/fxz/mall/order/controller/app/OrderController.java
@@ -2,7 +2,6 @@ package com.fxz.mall.order.controller.app;
 
 import com.fxz.common.core.enums.IBaseEnum;
 import com.fxz.common.mp.result.Result;
-import com.fxz.common.security.annotation.Ojbk;
 import com.fxz.mall.order.dto.OrderSubmitDto;
 import com.fxz.mall.order.enums.PayTypeEnum;
 import com.fxz.mall.order.service.impl.OrderServiceImpl;
@@ -21,43 +20,44 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class OrderController {
 
-	private final OrderServiceImpl orderService;
+    private final OrderServiceImpl orderService;
 
-	/**
-	 * 订单确认 → 进入创建订单页面
-	 * <p>
-	 * 获取购买商品明细、用户默认收货地址、防重提交唯一token 进入订单创建页面有两个入口，1：立即购买；2：购物车结算
-	 * @param skuId 直接购买必填，购物车结算不填
-	 * @return OrderConfirmVO
-	 */
-	@Ojbk
-	@PostMapping("/confirm")
-	public Result<OrderConfirmVo> confirm(@RequestParam(required = false) Long skuId) {
-		return Result.success(orderService.confirm(skuId));
-	}
+    /**
+     * 订单确认 → 进入创建订单页面
+     * <p>
+     * 获取购买商品明细、用户默认收货地址、防重提交唯一token 进入订单创建页面有两个入口，1：立即购买；2：购物车结算
+     *
+     * @param skuId 直接购买必填，购物车结算不填
+     * @return OrderConfirmVO
+     */
+    @PostMapping("/confirm")
+    public Result<OrderConfirmVo> confirm(@RequestParam(required = false) Long skuId) {
+        return Result.success(orderService.confirm(skuId));
+    }
 
-	/**
-	 * 订单提交
-	 */
-	@PostMapping("/submit")
-	public Result<OrderSubmitVo> submit(@RequestBody OrderSubmitDto orderSubmitDto) {
-		return Result.success(orderService.submit(orderSubmitDto));
-	}
+    /**
+     * 订单提交
+     */
+    @PostMapping("/submit")
+    public Result<OrderSubmitVo> submit(@RequestBody OrderSubmitDto orderSubmitDto) {
+        return Result.success(orderService.submit(orderSubmitDto));
+    }
 
-	/**
-	 * 订单支付
-	 * @param orderId 订单id
-	 * @param payType 支付方式
-	 * @param appId 小程序appId
-	 */
-	@PostMapping("/{orderId}/pay")
-	public <T> Result<T> pay(@PathVariable Long orderId, Integer payType, String appId) {
+    /**
+     * 订单支付
+     *
+     * @param orderId 订单id
+     * @param payType 支付方式
+     * @param appId   小程序appId
+     */
+    @PostMapping("/{orderId}/pay")
+    public <T> Result<T> pay(@PathVariable Long orderId, Integer payType, String appId) {
 
-		PayTypeEnum payTypeEnum = IBaseEnum.getEnumByValue(payType, PayTypeEnum.class);
-		if (payTypeEnum == null) {
-			return Result.failed("系统暂不支持该支付方式~");
-		}
-		return Result.success(orderService.pay(orderId, appId, payTypeEnum));
-	}
+        PayTypeEnum payTypeEnum = IBaseEnum.getEnumByValue(payType, PayTypeEnum.class);
+        if (payTypeEnum == null) {
+            return Result.failed("系统暂不支持该支付方式~");
+        }
+        return Result.success(orderService.pay(orderId, appId, payTypeEnum));
+    }
 
 }
